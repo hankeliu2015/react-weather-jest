be9dcab88e620c5640388c484ae682f3479abef7
github username <github email address>
2019-10-11 15:29:02 -0400

    install moment.js and add current time

--- src/components/currentForecast.js
+++ src/components/currentForecast.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment'
 
 const CurrentForecast = props => {
   // debugger
@@ -8,7 +9,8 @@ const CurrentForecast = props => {
       <h2>Current Forecast </h2>
       <div style={{border: 'solid 1 px black', padding: '16px', margin: '16px'}}>
         <h3>{props.forecast.currently.summary}</h3>
-          <p>Current Time: {props.forecast.currently.time}</p>
+          <p>Time Zone: {props.forecast.timezone}</p>
+          <p>Current Time: {moment().format('MMMM Do YYYY, h:mm:ss a')}</p>
           <p>Temperature: {props.forecast.currently.temperature}</p>
           <p>feels Like: {props.forecast.currently.apparentTemperature}</p>
           <p>Chance of Percipitation: {props.forecast.currently.precipProbability}</p>
