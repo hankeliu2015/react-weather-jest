6d56bfbce46b03736973183a185ed457d15f7d7f
github username <github email address>
2019-10-11 15:54:59 -0400

    refactor style sheet

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
             :
             <div>
-              <Navbar></Navbar>
+              <Navbar />
               <CurrentForecast forecast={this.state.weatherData} />
             </div>
           }
