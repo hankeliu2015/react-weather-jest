09dd98bb4e6e0b1ed5a3ef321a3d2cf2f87d543f
github username <github email address>
2019-10-14 19:37:56 -0400

    testing datalist in jsx on navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,6 +7,17 @@ const Navbar = ({changeForecast}) => {
       <button className={"nav-button"} onClick={()=> changeForecast('minutely')} >Minutely</button>
       <button className="nav-button" onClick={()=> changeForecast('hourly')} >Hourly</button>
       <button className="nav-button" onClick={()=> changeForecast('daily')} >Daily</button>
+
+        <input type="text" list='location' name="location" />
+        <datalist id="locaton">
+          <option value="New York" />
+          <option value="DC" />
+          <option value="Boston" />
+          <option value="Chicago" />
+          <option value="Denvor" />
+        </datalist>
+        <input type="submit" value="Submit Location" />
+        
     </div>
   )
 }
