abcb402d2f828f787cb8cd9615a5902dd3618213
github username <github email address>
2019-10-15 23:55:59 -0400

    add handleSubmit function to fetch opencase api, work in progress.

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,9 @@ import React, {Component} from 'react';
 
 class Navbar extends Component  {
   state = {
-    location: ''
+    location: '',
+    lng: '',
+    lat: ''
   }
 
   handleChange = (e) => {
@@ -10,8 +12,23 @@ class Navbar extends Component  {
       [e.target.name]: e.target.value
     })
   }
-  render () {
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const PLACENAME = this.state.location;
+    fetch(`https://api.opencagedata.com/geocode/v1/json?q=${PLACENAME}&key=${process.env.GEOCODING}`)
+    .then(resp => resp.json())
+    .then(location => this.setState({
+      lng: location.results.annotations.DMS.lng,
+      lat: location.results.annotations.DMS.lat
+    }))
+    console.log("location Data", this.state)
+    console.log("Open Cage key", process.env.GEOCODING)
+
+    //fetch the data from open openCage//
+    //pass in the lat/lang into callback function to fetch dark sky
+  }
+  render () {
     return (
       <div>
         <button className={"nav-button"} onClick={()=> this.props.changeForecast('currently')}>Currently</button>
@@ -19,15 +36,17 @@ class Navbar extends Component  {
         <button className="nav-button" onClick={()=> this.props.changeForecast('hourly')} >Hourly</button>
         <button className="nav-button" onClick={()=> this.props.changeForecast('daily')} >Daily</button>
 
-        <input type="text" list='location' name="location" value={this.state.location} onChange={this.handleChange}/>
-        <datalist id="locaton">
-          <option value="New York" />
-          <option value="DC" />
-          <option value="Boston" />
-          <option value="Chicago" />
-          <option value="Denvor" />
-        </datalist>
-        <input type="submit" value="Submit Location" />
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" list='location' name="location" value={this.state.location} onChange={this.handleChange}/>
+          <datalist id="locaton">
+            <option value="New York" />
+            <option value="DC" />
+            <option value="Boston" />
+            <option value="Chicago" />
+            <option value="Denvor" />
+          </datalist>
+          <input type="submit" value="Submit Location" />
+        </form>
 
       </div>
     )
@@ -39,7 +58,7 @@ class Navbar extends Component  {
 export default Navbar;
 
 {/*
-  use state.locaton to store the location data
+
   onSubmit will invoke handleSubmit(), launch fetch to openCage pass in location as argument. return the lan/lat.
   another callback function in app.js, take the lan/lat argment, fetch dark sky API.
   state.weatherData in App.js will be updated, pass into props to child components.
